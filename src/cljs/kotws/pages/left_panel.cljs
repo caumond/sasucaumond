db2c2925003a6f09d5505e245a4159b3748cc331
anthonycaumond <caumond@gmail.com>
2020-07-20 23:41:12 +0200

    Create submenus for blogs, Closes #9

--- src/cljs/kotws/pages/left_panel.cljs
+++ src/cljs/kotws/pages/left_panel.cljs
@@ -6,12 +6,17 @@
 (defn panel []
   [:ul
    {:class "left-panel"}
-   [re-com/label :label "coucou"]
-   [:li [re-com/hyperlink-href :label "Home" :href "#/"]]
-   [:li [re-com/hyperlink-href :label "Blog" :href "#/blog-cc-clojure"]]
+   [:li [:a {:href "#/"} "Home"]]
+   [:li {:class "dropdown"}
+    [:a {:href "#/blog-cc-clojure"} "Blog"]
+    [:div {:class "dropdown-content"}
+     [:a {:href "#"} "Link 1"]
+     [:a {:href "#"} "Link 2"]
+     ]
+    ]
    [:li [re-com/hyperlink-href :label "COI" :href "#/coi"]]
    [:li [re-com/hyperlink-href :label "CV" :href "#/cv"]]
-   [:li [re-com/hyperlink-href :label "Biblio" :href "#/biblio"]]
+   [:li [:a {:href "#/biblio"} "Biblio"]]
    [:li [re-com/hyperlink-href :label "About" :href "#/about"]]
    ]
   )
