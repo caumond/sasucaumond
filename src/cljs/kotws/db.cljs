e8da10ce46d0bad54ed1b1ab2b052a9bb8f2c3b9
caumond <anthony@caumond.com>
2024-11-04 15:47:26 +0100

    Save selected language in localstorage

--- src/cljs/kotws/db.cljs
+++ src/cljs/kotws/db.cljs
@@ -1,13 +1,19 @@
-(ns kotws.db "Default database values.")
+(ns kotws.db
+  "Default database values."
+  (:require [kotws.local-storage :as klocal-storage]))
 
-(defn from-page
-  "Returns the language as set in the current page."
+(defn lang-strategy
+  "Returns the language of the local storage, if not, get the accepted language from the server request."
   []
-  (let [l js/lang] (if l (keyword l) :en)))
+  (let [server-accepted-lang js/lang
+        ls-lang (klocal-storage/get-item :lang)]
+    (cond (some? ls-lang) (keyword ls-lang)
+          server-accepted-lang (keyword server-accepted-lang)
+          :else :en)))
 
 (def default-db
   {:name "Anthony CAUMOND professional website",
    :selected-items #{},
-   :language (from-page),
+   :language (lang-strategy),
    :active-panel nil,
    :slide-idx 0})
